86fa07089a321d0a28e59811eefb0359756aafee
JamesJetton <saasbook@saasbook.(none)>
2015-04-20 17:29:49 -0300

    Added calendar view/ still needs to be linked

--- app/models/performance_request.rb
+++ app/models/performance_request.rb
@@ -1,12 +1,12 @@
 class PerformanceRequest < ActiveRecord::Base
 
- attr_accessible :date, :description, :email, :first_name, :last_name, :location, :organization, :status, :kind, :dress, :adr_line1, :adr_line2, :city, :state, :zip
+ attr_accessible :start_at, :end_at, :description, :email, :first_name, :last_name, :location, :organization, :status, :kind, :dress, :adr_line1, :adr_line2, :city, :state, :zip
 
  validates :first_name, :presence => true
  validates :last_name, :presence => true
  validates :email, :presence => true
  validates :location, :presence => true
- validates :date, :presence => true
+ validates :start_at, :presence => true
  validates :description, :presence => true
  validates :kind, :presence => true
  validates :dress, :presence => true 
@@ -14,5 +14,7 @@ class PerformanceRequest < ActiveRecord::Base
  validates :city, :presence => true
  validates :state, :presence => true
  validates :zip, :presence => true
+
+ has_event_calendar
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150420182442) do
+ActiveRecord::Schema.define(:version => 20150413191530) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -31,20 +31,13 @@ ActiveRecord::Schema.define(:version => 20150420182442) do
   add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
   add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
 
-  create_table "events", :force => true do |t|
-    t.string   "name"
-    t.datetime "start_at"
-    t.datetime "end_at"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "performance_requests", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
     t.string   "organization"
-    t.datetime "date"
+    t.datetime "start_at"
+    t.datetime "end_at"
     t.string   "location"
     t.text     "description"
     t.string   "status"
