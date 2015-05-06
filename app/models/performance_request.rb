4b32a4e8b9fec9a857754e280cf36d166d47b31f
JamesJetton <saasbook@saasbook.(none)>
2015-05-06 01:28:36 -0300

    updated validation

--- app/models/performance_request.rb
+++ app/models/performance_request.rb
@@ -5,7 +5,7 @@ class PerformanceRequest < ActiveRecord::Base
  validates :first_name, :presence => true
  validates :last_name, :presence => true
  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
- #validates :number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "Not a valid phone number" } , :allow_blank => true
+ validates :number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "Should be in form ddd-ddd-dddd" } , :allow_blank => true
  validates :location, :presence => true
  validates :start_at, :presence => true
  validates :description, :presence => true
@@ -14,7 +14,8 @@ class PerformanceRequest < ActiveRecord::Base
  validates :adr_line1, :presence => true
  validates :city, :presence => true
  validates :state, :presence => true
- validates :zip, :presence => true
+ validates_format_of :p_zip, :with => /^\d{5}(-\d{4})?$/, :message => "should be in the form 12345 or 12345-1234", :allow_blank => true
+ validates_format_of :zip, :with => /^\d{5}(-\d{4})?$/, :message => "should be in the form 12345 or 12345-1234"
 
  has_event_calendar
 
